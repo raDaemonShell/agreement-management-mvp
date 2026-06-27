575597d05b4581149d6f47c8df547f8188dec1dc
raDaemonShell <yajra961990@gmail.com>
2026-06-27 10:50:51 +0800

    admin account

---
+++ backend/create_admin.py
@@ -0,0 +1,24 @@
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+import django
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(
+        username=username,
+        email=email,
+        password=password,
+    )
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
