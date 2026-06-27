64c33fa865b5ecbed9d7b4e1b47235f9cd7c9343
raDaemonShell <yajra961990@gmail.com>
2026-06-27 13:30:45 +0800

    add test weasyprint

--- backend/config/views.py
+++ backend/config/views.py
@@ -10,35 +10,38 @@ def home(request):
 
 from django.http import JsonResponse
 
+from agreements.models import Agreement
+from agreements.pdf import generate_agreement_pdf
+from django.conf import settings
 from django.http import HttpResponse
-from weasyprint import HTML
-import traceback
-
-from django.http import HttpResponse
-from weasyprint import HTML
-import traceback
+import os
 
 def test_pdf(request):
     try:
-        print("Starting WeasyPrint...")
+        agreement = Agreement.objects.first()
 
-        pdf = HTML(
-            string="<h1>Hello Render!</h1>"
-        ).write_pdf()
+        if agreement is None:
+            return HttpResponse("No agreement found in database.")
 
-        print("PDF generated successfully.")
+        generate_agreement_pdf(agreement)
 
-        return HttpResponse(
-            pdf,
-            content_type="application/pdf"
+        pdf_path = os.path.join(
+            settings.MEDIA_ROOT,
+            "agreements",
+            f"{agreement.id}.pdf"
         )
 
-    except Exception:
+        with open(pdf_path, "rb") as f:
+            return HttpResponse(
+                f.read(),
+                content_type="application/pdf"
+            )
+
+    except Exception as e:
+        import traceback
         traceback.print_exc()
-        return HttpResponse(
-            "WeasyPrint failed. Check Render logs.",
-            status=500
-        )
+        return HttpResponse(str(e), status=500)
+
 def check_weasyprint(request):
     return JsonResponse({
         "status": "WeasyPrint imported successfully"
