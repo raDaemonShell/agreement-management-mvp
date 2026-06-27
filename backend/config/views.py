fe9f20855d87c2c5879f4a985d87ff1734ad7c21
raDaemonShell <yajra961990@gmail.com>
2026-06-27 13:16:37 +0800

    add test weasyprint

--- backend/config/views.py
+++ backend/config/views.py
@@ -9,12 +9,32 @@ def home(request):
     )
 
 from django.http import JsonResponse
-from weasyprint import HTML
+
 from django.http import HttpResponse
+from weasyprint import HTML
+import traceback
 
 def test_pdf(request):
-    print("===== TEST PDF VIEW EXECUTED =====")
-    return HttpResponse("TEST PDF WORKS")
+    try:
+        print("Starting WeasyPrint...")
+
+        pdf = HTML(
+            string="<h1>Hello Render!</h1>"
+        ).write_pdf()
+
+        print("PDF generated successfully.")
+
+        return HttpResponse(
+            pdf,
+            content_type="application/pdf"
+        )
+
+    except Exception:
+        traceback.print_exc()
+        return HttpResponse(
+            "WeasyPrint failed. Check Render logs.",
+            status=500
+        )
 
 def check_weasyprint(request):
     return JsonResponse({
